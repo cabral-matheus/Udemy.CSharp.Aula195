c7fdea91ea6b20fb3371b9cff9fd6f90909b9c8b
Matheus Cabral <matheuscabraldesousa@gmail.com>
2023-06-19 15:38:55 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Starting the file manipulation studies in the C# course on the udemy.com website.
---
+++ Udemy.CSharp.Aula195/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Udemy.CSharp.Aula195
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"C:\Users\mathe\OneDrive\Documentos\Projetos C#\Udemy.CSharp.Aula195\file1.txt";
+
+            try
+            {
+                FileInfo fileInfo = new FileInfo(sourcePath);
+
+                string[] lines = File.ReadAllLines(sourcePath);
+
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            } catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
